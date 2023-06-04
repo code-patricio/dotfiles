2b2c269d69f1e05d683b6405047af63740225ab6
code-patricio <pat.gb.moreno@gmail.com>
2023-06-04 03:27:00 -0300

    set changes

--- nvim/lua/code-patricio/set.lua
+++ nvim/lua/code-patricio/set.lua
@@ -9,6 +9,7 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
+vim.opt.autoindent = true
 
 vim.opt.wrap = false
 
@@ -23,7 +24,7 @@ vim.opt.incsearch = true
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
+--vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
 --vim.opt.updatetime = 50
