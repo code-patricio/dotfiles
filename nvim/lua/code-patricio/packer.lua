510ad2d59ee0e09f982c5531513c1aae514cb5df
code-patricio <pat.gb.moreno@gmail.com>
2023-07-22 21:45:27 -0300

    batman

--- nvim/lua/code-patricio/packer.lua
+++ nvim/lua/code-patricio/packer.lua
@@ -7,23 +7,17 @@ return require('packer').startup(function(use)
 	-- Packer can manage itself
 	use 'wbthomason/packer.nvim'
    
---    use { "catppuccin/nvim", as = "catppuccin" }
+    use { "catppuccin/nvim", as = "catppuccin" }
 
---    use {
---        'nvim-treesitter/nvim-treesitter',
---        run = function()
---            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
---            ts_update()
---        end,
---    }
+--    use {'nvim-treesitter/nvim-treesitter'}
 
 	use {'mbbill/undotree'}
 
 	use({'jakewvincent/mkdnflow.nvim'})
     
---    use {'theprimeagen/vim-be-good'}
-
     use {'jbyuki/venn.nvim'}
+
+--    use {'theprimeagen/vim-be-good'}
 
 --[[ 
 -- Not Lsp Zero
