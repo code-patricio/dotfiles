a44d68fb7b4a2d51229a4cab44dd27cd109abfdf
code-patricio <pat.gb.moreno@gmail.com>
2023-09-29 22:12:39 -0300

    lazy

--- nvim/lua/code-patricio/lazy.lua
+++ nvim/lua/code-patricio/lazy.lua
@@ -1,17 +1,5 @@
-local lazypath = vim.fn.stdpath("data") .. "lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
 
-requiere("lazy").setup({
+require("lazy").setup({
 
 	"jakewvincent/mkdnflow.nvim",
 
