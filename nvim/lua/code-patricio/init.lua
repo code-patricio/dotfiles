a74df1c9aae9ae64d90af41120b602000d2e3974
code-patricio <pat.gb.moreno@gmail.com>
2023-09-29 22:08:24 -0300

    lazy

--- nvim/lua/code-patricio/init.lua
+++ nvim/lua/code-patricio/init.lua
@@ -2,6 +2,19 @@ require("code-patricio.set")
 require("code-patricio.remap")
 require("code-patricio.abbrev")
 
+local lazypath = vim.fn.stdpath("data") .. "lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
 -- If you have an init.lua
 vim.api.nvim_create_autocmd("FileType", {pattern = "markdown", command = "set awa"})
 -- Use the following if your buffer is set to become hidden
